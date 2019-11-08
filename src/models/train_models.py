27f6b5f163c8b9341361e3508948c8d123e0ed65
jejjohnson <emanjohnson91@gmail.com>
2019-11-08 13:54:21 +0100

    Modified train_rbig function. Added MI with batches.

--- src/models/train_models.py
+++ src/models/train_models.py
@@ -2,12 +2,16 @@ import sys
 
 sys.path.insert(0, "/home/emmanuel/code/rbig")
 
-from rbig import RBIG, RBIGMI, RBIGKLD
+from rbig import RBIG
+from src.models.information.mutual_information import RBIGEstimator as RBIGMI
+
 import numpy as np
 import time
 
 
-def run_rbig_models(X1, X2=None, measure="t", verbose=None, random_state=123):
+def run_rbig_models(
+    X1, X2=None, measure="t", verbose=None, random_state=123, batch_size=10_000
+):
 
     # RBIG Parameters
     n_layers = 10000
@@ -60,6 +64,7 @@ def run_rbig_models(X1, X2=None, measure="t", verbose=None, random_state=123):
             pdf_extension=pdf_extension,
             pdf_resolution=pdf_resolution,
             verbose=verbose,
+            batch_size=batch_size,
         )
 
         # fit model to the data
@@ -72,41 +77,11 @@ def run_rbig_models(X1, X2=None, measure="t", verbose=None, random_state=123):
                 f"Trained RBIG1 MI ({X1.shape[0]:,} points, {X1.shape[1]:,} dimensions): {t1:.3f} secs"
             )
 
-        mi = rbig_mi_model.mutual_information() * np.log(2)
+        mi = rbig_mi_model.score(X1)
         if verbose:
             print(f"MI: {mi:.3f}")
 
         return mi, t1
-
-    elif measure.lower() == "kld":
-
-        rbig_kld_model = RBIGKLD(
-            n_layers=n_layers,
-            rotation_type=rotation_type,
-            random_state=random_state,
-            zero_tolerance=zero_tolerance,
-            tolerance=tolerance,
-            pdf_extension=pdf_extension,
-            pdf_resolution=pdf_resolution,
-            verbose=verbose,
-        )
-
-        # fit model to the data
-        t0 = time.time()
-        rbig_kld_model.fit(X1, X2)
-        t1 = time.time() - t0
-
-        if verbose:
-            print(
-                f"Trained RBIG KLD ({X1.shape[0]:,}, {X2.shape[0]:,} points): {t1:.3f} secs"
-            )
-
-        kld = rbig_kld_model.kld * np.log(2)
-        
-        if verbose:
-            print(f"KLD: {kld:.3f}")
-
-        return kld, t1
 
     else:
         raise ValueError(f"Unrecognized measure: {measure}.")
