d52bcdf05ac969638e19bfc5814e70cf00bd5a27
jejjohnson <emanjohnson91@gmail.com>
2019-11-08 12:42:31 +0100

    updated the batch class. more arguments and comments.

--- src/models/information/ensemble.py
+++ src/models/information/ensemble.py
@@ -18,8 +18,47 @@ class Ensemble:
 
 
 class Batch:
-    def __init__(self, random_state: int = 123):
+    """Abstract class to be used to estimate scores in batches.
+    
+    Parameters
+    ----------
+    batch_size : int, default = 1_000
+        batch size
+    
+    min_batch_size : int, default = 100
+        the minimum batch size to be used for the indices generator
+
+    shuffle : bool, default = True
+        option to shuffle the data before doing batches
+
+    random_state : int, default = None
+        the random seed when doing the shuffling if option chosen
+
+    summary : str, default = 'mean'
+        the way to summarize the scores {'mean', 'median'}
+    
+    Attributes
+    ----------
+    batch_scores : np.ndarray
+        the raw batchsize scores
+
+    score : float
+        the final score after the summary stat (e.g. mean)
+    """
+
+    def __init__(
+        self,
+        batch_size: int = 1_000,
+        min_batch_size: int = 100,
+        shuffle: bool = True,
+        random_state: int = 123,
+        summary: str = "mean",
+    ):
+        self.batch_size = batch_size
+        self.min_batch_size = min_batch_size
         self.random_state = random_state
+        self.shuffle = shuffle
+        self.summary = summary
 
     def _fit(self, X: np.ndarray, y: Optional[np.ndarray] = None):
         """
@@ -27,9 +66,7 @@ class Batch:
         """
         pass
 
-    def _fit_batches(
-        self, X: np.ndarray, Y: Optional[np.ndarray] = None, batch_size: int = 1_000
-    ) -> float:
+    def _fit_batches(self, X: np.ndarray, Y: Optional[np.ndarray] = None) -> float:
         """
         Fits models to inherited class
 
@@ -41,19 +78,38 @@ class Batch:
         y : np.ndarray
             The second dataset to be fit
 
-        batch_size : int, default=1_000
-            The batchsize to generate the datasets
+        Returns
+        -------
+        score : float
+            the score after the summary
         """
         it_measure = list()
 
-        if Y is not None:
-            X, Y = shuffle(X, Y, random_state=self.random_state)
-        else:
-            X = shuffle(X, random_state=self.random_state)
-        for idx in gen_batches(X.shape[0], batch_size, 10):
+        # Shuffle dataset
+        if self.shuffle:
+            if Y is not None:
+                X, Y = shuffle(X, Y, random_state=self.random_state)
+            else:
+                X = shuffle(X, random_state=self.random_state)
+
+        # batch scores
+        for idx in gen_batches(X.shape[0], self.batch_size, self.min_batch_size):
             if Y is not None:
                 it_measure.append(self._fit(X[idx], Y[idx]))
             else:
                 it_measure.append(self._fit(X[idx]))
 
-        return np.mean(it_measure)
+        # save raw scores
+        self.batch_scores = np.ndarray(it_measure)
+
+        # return summary score
+        if self.summary == "mean":
+            self.score = np.mean(it_measure)
+
+        elif self.summary == "median":
+            self.score = np.median(it_measure)
+
+        else:
+            raise ValueError("Unrecognized summarizer: {}".format(self.summary))
+
+        return self.score
